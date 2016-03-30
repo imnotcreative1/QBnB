25ad6762a1f6fd865f0556df73abffc3e913c93d
Patrick Dang-Ho <patrickdangho@gmail.com>
2016-03-30 04:06:57 -0400

    added delete property script

---
+++ deleteProperty.php
@@ -0,0 +1,30 @@
+<?php
+
+include ('config.php');
+session_start();
+if (isset($_GET['propertyAddress']) && isset($_SESSION['email'])) {
+
+$propertyAddress = $_GET['propertyAddress'];
+
+$query1 = "DELETE from booking where booking.ID in (SELECT id from availability NATURAL join property where property.address = ?);";
+	$stmt = $con->prepare($query1);
+	$stmt->bind_param("s", $propertyAddress);
+	$stmt->execute();
+
+$query2 = "DELETE from availability where availability.address = ?";
+	$stmt = $con->prepare($query2);
+	$stmt->bind_param("s", $propertyAddress);
+	$stmt->execute();
+
+$query3 = "DELETE from property where address = ?";
+	$stmt = $con->prepare($query3);
+	$stmt->bind_param("s", $propertyAddress);
+	$stmt->execute();
+
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+
+} else {
+	header('Location: index.php');
+}
+
+?>
